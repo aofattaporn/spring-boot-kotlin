6f5dd8ff9af22e8fa6da2c90063376c8fea0205b
aofattaporn <aofattapon321@gmail.com>
2023-08-27 15:31:00 +0700

    add post method

--- src/main/kotlin/com/playground/demo/controllers/BlogControllers.kt
+++ src/main/kotlin/com/playground/demo/controllers/BlogControllers.kt
@@ -3,14 +3,23 @@ package com.playground.demo.controllers
 import com.playground.demo.models.Blog
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BlogControllers {
 
     @GetMapping("/")
-    fun blog(): ResponseEntity<Blog> {
+    fun getAllPost(): ResponseEntity<Blog> {
         val blog =  Blog(10, "T", "Hello world");
+
+        return ResponseEntity.ok(blog);
+    }
+
+    @PostMapping("/")
+    fun createPost(@RequestBody requestBody: Blog  ): ResponseEntity<Blog> {
+        val blog =  Blog(requestBody.age, requestBody.name, requestBody.email);
 
         return ResponseEntity.ok(blog);
     }
